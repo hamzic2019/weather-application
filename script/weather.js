a7a8da2d7d087ea90c038ab7c1fc3874594606dd
Haris Hamzic <hamzicba@icloud.com>
2020-08-20 01:03:51 +0200

    hidden api key in api.js and added .gitignore

--- script/weather.js
+++ script/weather.js
@@ -1,7 +1,3 @@
-const apiKey = 'miWGR9IGJiXS9wjaFAunXaPRah4snCSR';
-// Usually apiKay has to be hidden, but this one I let be public, because it has limit to the max 50 calls a day!
-
-
 const getCityCode = async (city) => {
     const url = `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${city}`;
     const code = await fetch(url);
