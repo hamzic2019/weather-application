a95e29adc182fb8636637fa265a5f1ca3c537ecc
Haris Hamzic <hamzicba@icloud.com>
2020-08-20 00:34:23 +0200

    adding https to urls

--- script/weather.js
+++ script/weather.js
@@ -3,13 +3,13 @@ const apiKey = 'miWGR9IGJiXS9wjaFAunXaPRah4snCSR';
 
 
 const getCityCode = async (city) => {
-    const url = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${city}`;
+    const url = `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${city}`;
     const code = await fetch(url);
     return code.json();
 };
 
 const getCityWeather = async (key) => {
-    const url = `http://dataservice.accuweather.com/currentconditions/v1/${key.Key}?apikey=${apiKey}`;
+    const url = `https://dataservice.accuweather.com/currentconditions/v1/${key.Key}?apikey=${apiKey}`;
     const weatherData = await fetch(url);
     return weatherData.json();
 }
