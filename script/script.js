1c0a18bce9932d650a3d999f7f7a0c459bf86e03
Haris Hamzic <hamzicba@icloud.com>
2020-08-20 21:46:06 +0200

    changed functional js to the OOP

--- script/script.js
+++ script/script.js
@@ -4,29 +4,23 @@ const button = document.querySelector('.submitButton');
 const displayCity = document.querySelector('.targetCity');
 const displayIcon = document.querySelector('.weatherIcon');
 const displayTemp = document.querySelector('.cityTempreature');
-let cityName = '';
+const forcast = new Forcast();
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const data = form.children[1].value;
     
-    if(data.length > 0) {
-        const city = e.target[0].value;
-        getCityCode(city)
+    if(data.length > 0) {        
+        forcast.displayUI(e.target[0].value)
             .then(e => {
-                cityName = e[0].LocalizedName;
-                return getCityWeather(e[0]);
-            })
-            .then(e => {
-                const temp = e[0].Temperature.Metric.Value;
-
-                displayCity.innerText = cityName;
-                displayIcon.setAttribute('src', `imgs/icons/${e[0].WeatherIcon}.svg`);
-                displayTemp.innerText = `${temp}°C`;
+                displayCity.innerText = e.cityName;
+                displayIcon.setAttribute('src', `imgs/icons/${e.icon}.svg`);
+                displayTemp.innerText = `${e.temp}°C`;
             })
             .catch(err => {
-                console.log(err);
+
             })
     }
+
 });
 
--- script/weather.js
+++ script/weather.js
@@ -1,15 +1,28 @@
-const apiKey = 'miWGR9IGJiXS9wjaFAunXaPRah4snCSR';
-// Usually apiKay has to be hidden, but this one I let be public, because it has limit to the max 50 calls a day!
+class Forcast {
+    constructor(){
+        this.apiKey = 'hGXxbS57NBhYV9Keuph0xuQAzR9s3hqj';
+    }
 
+    async displayUI(city){
+        const data = await this.getCityCode(city);
+        const weather = await this.getCityWeather(data[0]);
+        return {
+            cityName: data[0].LocalizedName,
+            temp: weather[0].Temperature.Metric.Value,
+            icon: weather[0].WeatherIcon
+        }
+    }
 
-const getCityCode = async (city) => {
-    const url = `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${city}`;
-    const code = await fetch(url);
-    return code.json();
-};
+    async getCityCode(city) {
+        const url = `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${this.apiKey}&q=${city}`;
+        const code = await fetch(url);
+        return code.json();
+    }
 
-const getCityWeather = async (key) => {
-    const url = `https://dataservice.accuweather.com/currentconditions/v1/${key.Key}?apikey=${apiKey}`;
-    const weatherData = await fetch(url);
-    return weatherData.json();
+    async getCityWeather(key) {
+        const url = `https://dataservice.accuweather.com/currentconditions/v1/${key.Key}?apikey=${this.apiKey}`;
+        const weatherData = await fetch(url);
+        return weatherData.json();
+    }
 }
+
